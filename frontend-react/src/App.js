6a468fd497dfc605decb65a3667c1856b4101c54
pavel ilin <refugeebase@gmail.com>
2020-02-16 15:27:36 -0500

    frontend init

---
+++ frontend-react/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    dataLoaded: false,
+    books: [],
+  }
+
+  fetchData = () => {
+    fetch('http://localhost:3000/books')
+    .then(r => r.json())
+    .then(resp => {
+      this.setState({
+        dataLoaded: true,
+        books: resp
+      })
+    })
+  }
+
+  renderBooks = () => {
+    let tags = []
+
+    return this.state.books.map((book) => {
+
+      book.tags.map((tag) => {
+        tags.push(tag.title)
+      })
+
+      return (
+        <div>
+          <p>Title: {book.title}</p>
+          <p>Author: {book.author}</p>
+          <p>Publication year: {book.publication_year}</p>
+          <p>Edited: {book.edited ? 'Yes' : 'No'}</p>
+          <p>Tags: {tags.join(', ')}</p>
+        </div>
+      )
+    })
+  }
+
+  render(){
+    return(
+        <div>
+          {this.state.dataLoaded ? null : this.fetchData()}
+          {this.state.dataLoaded ? this.renderBooks() : null }
+        </div>
+    )
+  }
+}
+
+export default App;
